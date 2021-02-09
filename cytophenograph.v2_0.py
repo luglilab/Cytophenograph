9d43b2880d0ff8f6418c9ad3cbe5108c08107af8
Simone Puccio <simone.puccio@humanitasresearch.it>
2021-02-09 20:37:51 +0100

    Update cytophenograph.v2_0.py

--- cytophenograph.v2_0.py
+++ cytophenograph.v2_0.py
@@ -2,17 +2,16 @@ import optparse
 from PhenoFunctions_v2_0 import *
 import os
 import pandas as pd
-parser = optparse.OptionParser(usage='', version='1.0')
-parser.add_option('-i', action="store", dest="input_folder", help='')
-parser.add_option('-o', action="store", dest="output_folder", help='')
-parser.add_option('-k', action="store", dest="kmeancoef", help='')
-# parser.add_option('-k', action="append", dest='kmeancoef', default=[])
-parser.add_option('-m', action="store", dest="markerlist", help='')
-parser.add_option('-n', action="store", dest="analysis_name", help='')
-parser.add_option('-t', action="store", dest="thread", help='')
-parser.add_option('-p', action="store", dest="pheno", help='')
+parser = optparse.OptionParser(usage='python ./Cytophenograph/cytophenograph.v2_0.py -i $abs_path/Cytophenograph/Test_dataset/CD8_Panel_II_channelvalues_GA_downSampled/ -o $abs_path/Cytophenograph/output_test -k 300 -m $abs_path/Cytophenograph/Test_dataset/CD8_bulk_markers_to_exclude.txt -n Test -t 10 -p $abs_path/Cytophenograph/Test_dataset/Info_file_bulk_Test.xlsx -c Parc', version='2.0')
+parser.add_option('-i', action="store", dest="input_folder", help='Absolute path of folder with CSV files.')
+parser.add_option('-o', action="store", dest="output_folder", help='Absolute path of output folder. TIPS: Please use an empty folder.')
+parser.add_option('-k', action="store", dest="kmeancoef", help='Number for nearest neighbors search for Phenograph execution.')
+parser.add_option('-m', action="store", dest="markerlist", help='Text file with features(channel name) to exclude during clustering execution.')
+parser.add_option('-n', action="store", dest="analysis_name", help='Analysis name.')
+parser.add_option('-t', action="store", dest="thread", help='Number of jobs.')
+parser.add_option('-p', action="store", dest="pheno", help='Excel file with the following columns "Sample	Cell_type	EXP	ID	Time_point	Condition	Count", that will be integrated as metadata.')
 parser.add_option('-c', type='choice', choices=['Phenograph', 'Parc', 'Both'],
-                  dest="clustering",default = "Phenograph", help='')
+                  dest="clustering",default = "Phenograph", help='Tool selecting for clustering. Both will execute Phenograph and Parc.')
 parser.add_option('-d', type='choice', choices=[True, False],dest="tsne",default = False, help='')
 options, args = parser.parse_args()
 
