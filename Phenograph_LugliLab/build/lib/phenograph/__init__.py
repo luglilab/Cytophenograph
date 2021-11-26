80add43981e81d66f19587a42b6180d5a522be63
sinnamone <sinnamone@gmail.com>
2021-11-26 14:12:58 +0100

    Update

--- Phenograph_LugliLab/build/lib/phenograph/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cluster import cluster
-from .classify import classify
-from .version import __version__
--- Phenograph_LugliLab/phenograph/version.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.5.7"
-__author__ = "Jacob Levine"
-__email__ = "jl3545@columbia.edu"
