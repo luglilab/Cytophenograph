682a7dcade130d8664da806e04f9480d115c42a7
LugliLab <simone.puccio@humanitasresearch.it>
2021-12-01 18:26:09 +0100

    Update cytophenograph.v3_0.py

--- cytophenograph.v3_0.py
+++ cytophenograph.v3_0.py
@@ -5,14 +5,14 @@ from PhenoFunctions_v3_0 import *
 parser = optparse.OptionParser(usage='python ./Cytophenograph/cytophenograph.v2_0.py -i $abs_path/Cytophenograph/Test_dataset/CD8_Panel_II_channelvalues_GA_downSampled/ -o $abs_path/Cytophenograph/output_test -k 300 -m $abs_path/Cytophenograph/Test_dataset/CD8_bulk_markers_to_exclude.txt -n Test -t 10 -p $abs_path/Cytophenograph/Test_dataset/Info_file_bulk_Test.xlsx -c Parc', version='2.0')
 parser.add_option('-i', action="store", dest="input_folder", help='Absolute path of folder with CSV files.')
 parser.add_option('-o', action="store", dest="output_folder", help='Absolute path of output folder. TIPS: Please use an empty folder.')
-parser.add_option('-k', action="store", dest="kmeancoef", help='Number for nearest neighbors search for Phenograph execution.')
+parser.add_option('-k', action="store", dest="kmeancoef", help='Number for nearest neighbors search for Phenograph execution.',default = "30")
 parser.add_option('-m', action="store", dest="markerlist", help='Text file with features(channel name) to exclude during clustering execution.')
 parser.add_option('-n', action="store", dest="analysis_name", help='Analysis name.')
 parser.add_option('-t', action="store", dest="thread", help='Number of jobs.')
 parser.add_option('-p', action="store", dest="pheno", help='Excel file with the following columns "Sample-Cell_type-EXP-ID-Time_point-Condition-Count", that will be integrated as metadata.')
 parser.add_option('-c', type='choice', choices=['Phenograph', 'Parc', 'Flowsom'],
                   dest="clustering",
-                  default = "Phenograph", help='Tool selecting for clustering. Both will execute Phenograph and Parc.')
+                  default = "Phenograph", help='Tool selecting for clustering, Selection available are Phenograph, Parc, Flowsom .')
 parser.add_option("-s", action="store_true", dest="scale", default=False, help='Apply MinMaxScaler(1-100) before clustering.')
 options, args = parser.parse_args()
 
