0585d5c05bdffd9124fd0ab904ddddf52ad82145
LugliLab <simone.puccio@humanitasresearch.it>
2022-07-15 11:21:22 +0200

    Add files via upload

--- cytophenograph.v5.py
+++ cytophenograph.v5.py
@@ -5,16 +5,16 @@ from PhenoFunctions_v5 import *
 parser = optparse.OptionParser(usage='python ./Cytophenograph/cytophenograph.v5.py -i $abs_path/Cytophenograph/Test_dataset/CD8_Panel_II_channelvalues_GA_downSampled/ -o $abs_path/Cytophenograph/output_test -k 300 -m $abs_path/Cytophenograph/Test_dataset/CD8_bulk_markers_to_exclude.txt -n Test -t 10 -p $abs_path/Cytophenograph/Test_dataset/Info_file_bulk_Test.xlsx -c VIA', version='5.0')
 parser.add_option('-b', action="store_true", dest="batch", default=False,
                   help='Perform batch correction with Scanorama.')
-parser.add_option('-c', type='choice', choices=['Phenograph', 'VIA', 'Flowsom'],
+parser.add_option('-c', type='choice', choices=['Phenograph', 'VIA', 'FlowSOM'],
                   dest="clustering",
-                  default="Phenograph", help='Tool selecting for clustering, Selection available are [Phenograph,VIA,Flowsom].')
-parser.add_option('-d', action="store", dest="mindist", default=0.5,type=float, help='min_dist parameter for UMAP generation')
+                  default="Phenograph", help='Tool selecting for clustering, Selection available are [Phenograph, VIA, FlowSOM].')
+parser.add_option('-d', action="store", dest="mindist", default=0.5, type=float, help='min_dist parameter for UMAP generation')
 parser.add_option('-e', type='choice', choices=['Sample', 'Cell_type','ID', 'EXP', 'Time_point', 'Condition'],
                   dest="batchcov",
                   default="Sample", help='Please, specify covariate to correct with Scanorama')
-parser.add_option('-f', type='choice', choices=['all', 'ceil', 'fixed'],
+parser.add_option('-f', type='choice', choices=['All', 'Balanced', 'Fixed'],
                   dest="downsampling",
-                  default="all", help='Concatenation Method.Available options are all, min,ceil, fixed.')
+                  default="All", help='Concatenation Method. Available options are All, Balaced, Fixed.')
 parser.add_option('-g', action="store", dest="cellnumber", default=1000, type=int, help='Number of event to for downsampling.')
 parser.add_option('-i', action="store", dest="input_folder", help='Absolute path of folder with CSV files.')
 parser.add_option('-k', action="store", dest="kmeancoef", help='Number for nearest neighbors search for Phenograph execution.Deafult value is 30',
@@ -24,28 +24,24 @@ parser.add_option('-n', action="store", dest="analysis_name", help='Analysis nam
 parser.add_option('-o', action="store", dest="output_folder", help='Absolute path of output folder. TIPS: Please use an empty folder.')
 parser.add_option('-p', action="store", dest="pheno", default="Condition",
                   help='Excel file with the following columns "Sample-Cell_type-EXP-ID-Time_point-Condition-Count", that will be integrated as metadata.')
-parser.add_option('-r', type='choice', choices=['full', 'umap', 'clustering'],
+parser.add_option('-r', type='choice', choices=['Full', 'UMAP', 'Clustering'],
                   dest="runtime",
-                  default="full", help='Runtime option for custom analysis. The available options are full , umap , clustering ')
-parser.add_option('-s', action="store", dest="spread", default=1,type=float, help='spread parameter for UMAP generation')
-parser.add_option('-t', action="store", dest="thread",type=int,default=1, help='Number of jobs.')
-parser.add_option('-w', action="store", dest="knn", help='Number of K-Nearest Neighbors for VIA KNN graph.Min allowed value is 5,Max allowed value is 100. Deafult value is 30',
+                  default="Full", help='Runtime option for custom analysis. The available options are Full, UMAP, Clustering.')
+parser.add_option('-s', action="store", dest="spread", default=1.0, type=float, help='spread parameter for UMAP generation')
+parser.add_option('-t', action="store", dest="thread", type=int, default=1, help='Number of jobs.')
+parser.add_option('-w', action="store", dest="knn", help='Number of K-Nearest Neighbors for VIA KNN graph. Min allowed value is 5, Max allowed value is 100. Deafult value is 30',
                   type=int, default=30)
-parser.add_option('-x', action="store", dest="minclus", help='Flowsom Min proposed number of clusters.Min allowed value is 5,Max allowed value is maxclus-2. Deafult value is 5',
+parser.add_option('-x', action="store", dest="minclus", help='FlowSOM Min proposed number of clusters. Min allowed value is 5, Max allowed value is maxclus-2. Deafult value is 5',
                   type=int, default=5)
-parser.add_option('-y', action="store", dest="maxclus", help='Flowsom Max proposed number of clusters.Min allowed value is minclus-2,Max allowed value is 31. Deafult value is 31',
+parser.add_option('-y', action="store", dest="maxclus", help='FlowSOM Max proposed number of clusters. Min allowed value is minclus-2, Max allowed value is 31. Default value is 31',
                   type=int, default=31)
-parser.add_option('-z', action="store", dest="resolution", help='A parameter value controlling the coarseness of the VIA clustering.Min allowed value is 0.2,Max allowed value is 1.5. Deafult value is 1.0',
+parser.add_option('-z', action="store", dest="resolution", help='A parameter value controlling the coarseness of the VIA clustering. Min allowed value is 0.2, Max allowed value is 1.5. Deafult value is 1.0',
                   type=float, default=1.0)
-
 
 options, args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    print("Script name: cytophenograph" )
-    print("Script version:", __version__)
-    print("Start")
     DictInfo = dict()
 
     run = Cytophenograph(info_file=options.pheno,
@@ -67,29 +63,29 @@ if __name__ == '__main__':
                          maxclus=options.maxclus,
                          downsampling=options.downsampling,
                          cellnumber=options.cellnumber)
-    DictInfo["Infofile"] = run.read_info_file()
-    DictInfo["List_csv_files"] = run.import_all_event()
-    DictInfo["adata_conc"] = run.concatenate_dataframe(DictInfo["Infofile"],
-                                                       DictInfo["List_csv_files"])
-    DictInfo["pathmarkerfile"], DictInfo["basenamemarkerfilepath"] = run.loadmarkers()
-    DictInfo["markertoexclude"] = run.checkmarkers()
-    DictInfo["markertoinclude"] = run.splitmarker()
-    if options.runtime != 'umap':
-        if options.clustering == "Phenograph":
-            print("Clustering tool selected is: Phenograph")
-            DictInfo["phenograph_adata"] = run.runphenograph()
-        elif options.clustering == "VIA":
-            print("Clustering tool selected is: VIA")
-            DictInfo["via_adata"] = run.runvia()
-        elif options.clustering == "Flowsom":
-            print("Clustering tool selected is: Flowsom")
-            DictInfo["flowsom_adata"] = run.runflowsom()
-        run.groupbycluster()
-        run.groupbysample()
-        run.exporting()
-    if options.runtime == 'umap':
-        print("UMAP generaration skipping clustering")
-        run.runtimeumap()
-        run.groupbysample()
-        run.exporting()
-    print("End")
+    try:
+        DictInfo["Infofile"] = run.read_info_file()
+        DictInfo["List_csv_files"] = run.import_all_event()
+        DictInfo["adata_conc"] = run.concatenate_dataframe(DictInfo["Infofile"],
+                                                           DictInfo["List_csv_files"])
+        DictInfo["pathmarkerfile"], DictInfo["basenamemarkerfilepath"] = run.loadmarkers()
+        DictInfo["markertoexclude"] = run.checkmarkers()
+        DictInfo["markertoinclude"] = run.splitmarker()
+        if options.runtime != 'UMAP':
+            if options.clustering == "Phenograph":
+                DictInfo["phenograph_adata"] = run.runphenograph()
+            elif options.clustering == "VIA":
+                DictInfo["via_adata"] = run.runvia()
+            elif options.clustering == "FlowSOM":
+                DictInfo["flowsom_adata"] = run.runflowsom()
+            run.groupbycluster()
+            run.groupbysample()
+            run.exporting()
+        if options.runtime == 'UMAP':
+            run.runtimeumap()
+            run.groupbysample()
+            run.exporting()
+    except Exception:
+        print(traceback.format_exc())
+        run.log.error("Execution Error!")
+        sys.exit(1)
